157fe45399d870ae7dd009a259edfbc7566793ad
Facundo Saavedra <facusaavedrx@gmail.com>
2023-06-19 15:20:28 -0300

    Se agrego Contenedor, Imagen y mas contenido al Heading con styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
 import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
 
+const Contenedor = styled.div`
+  max-width: 900px;
+  width: 90%;
+  margin: 0 auto;
+
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
   color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  font-size: 34px;
+  margin-top: 80px;
+  margin-bottom: 50px;
+
+  &::after {
+    content:'';
+    width: 50%;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `
 
 function App () {
   return (
-    <div className='App'>
-      <Heading>Hola mundo desde App</Heading>
-    </div>
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt='Imagenes de distintas criptomonedas'
+      />
+      <div>
+        <Heading>Cotizador de criptomonedas</Heading>
+      </div>
+    </Contenedor>
   )
 }
 
