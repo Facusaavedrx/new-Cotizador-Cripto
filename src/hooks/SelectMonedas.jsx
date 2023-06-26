c2a35e18d28ab02b49e537b731ce8d8bad9afa05
Facundo Saavedra <facusaavedrx@gmail.com>
2023-06-26 10:08:51 -0300

    Styled components a SelectMonedas.jsx

--- src/hooks/SelectMonedas.jsx
+++ src/hooks/SelectMonedas.jsx
@@ -2,19 +2,33 @@ import styled from '@emotion/styled'
 
 const Label = styled.label`
   color: #FFF;
+  display: block;
+  font-family: 'Lato', sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  margin: 15px 0;
 `
+const Select = styled.select`
+  width: 100%;
+  font-size: 18px;
+  padding: 14px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+`
+
 function SelectMonedas ({ label, opciones }) {
   return (
     <>
       <Label> {label} </Label>
-      <select>
+      <Select>
         <option value=''>Nombre:</option>
         {opciones.map(opcion => {
           return (
             <option value={opcion.id} key={opcion.id}> {opcion.nombre} </option>
           )
         })}
-      </select>
+      </Select>
     </>
   )
 }
