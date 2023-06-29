aa50339b64c9c90e0353b35c4781481f80ddf831
Facundo Saavedra <facusaavedrx@gmail.com>
2023-06-29 20:46:36 -0300

    Spinner agregado mientras carga

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import Formulario from './components/Formulario'
+import Spinner from './components/Spinner'
 import ImagenCripto from './img/imagen-criptos.png'
 import Resultado from './components/Resultado'
+
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -43,16 +45,19 @@ const Heading = styled.h1`
 function App () {
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargado] = useState(false)
 
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
       const cotizarCripto = async () => {
+        setCargado(true)
         const { moneda, cripto } = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cripto}&tsyms=${moneda}`
         const respuesta = await fetch(url)
         const resultado = await respuesta.json()
 
         setResultado(resultado.DISPLAY[cripto][moneda])
+        setCargado(false)
       }
 
       cotizarCripto()
@@ -74,6 +79,7 @@ function App () {
         />
 
         {resultado.PRICE && <Resultado resultado={resultado} />}
+        {cargando && <Spinner>Cargando</Spinner>}
       </div>
     </Contenedor>
   )
